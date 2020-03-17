8c71a70af488bbc7d0dfd0c105e45048b339c54e
moritzjuergens <moritzjuergens@outlook.de>
2020-03-17 18:17:11 +0100

    Changed both Hashsets to one HashMap Started the implementation of a Single Chat Option (SCO) Created connectIterate to clean up code

--- Verteilte Systeme/src/Server/HandlerThread.java
+++ Verteilte Systeme/src/Server/HandlerThread.java
@@ -2,6 +2,8 @@ package Server;
 
 import java.net.*;
 import java.io.*;
+import java.util.Iterator;
+import java.util.Map;
 
 
 class HandlerThread implements Runnable{
@@ -25,9 +27,9 @@ class HandlerThread implements Runnable{
                 if(name.isEmpty()){
                     return;
                 }
-                synchronized (Server.loginHash) {
-                    if(!Server.loginHash.contains(name) && !name.isBlank()) {
-                        Server.loginHash.add(name);
+                synchronized (Server.connect) {
+                    if(!Server.connect.containsKey(name) && !name.isBlank()) {
+                        Server.connect.put(name,out);
                         out.println("CONFIRMED");
                         System.out.println("User: '" + name + "' has logged in");
                         break;
@@ -35,26 +37,44 @@ class HandlerThread implements Runnable{
                 }
             }
 
-            Server.printHash.add(out);
+            Server.connect.get(name).println("MESSAGEFriends online:");
+            connectIterate("");
 
             while (true) {
                 String input = in.readLine();
                 System.out.println(name + ": " + input);
-                if (input.equals("GOODBYE")){
-                    System.out.println("'"+name+"' has logged off");
-                    for (PrintWriter printer : Server.printHash){
-                        printer.println("MESSAGE'"+name+"' has logged off");
-                    }
-                    socket.close();
-                }else {
-                    for (PrintWriter printer : Server.printHash) {
-                        printer.println("MESSAGE" + name+": "+input);
-                    }
-                }
-        }
 
+                if (input.equals("GOODBYE")) {
+                    System.out.println("'" + name + "' has logged off");
+                    connectIterate("has logged off...");
+                    socket.close();
+                }else if(input.contains("/#")&&input.contains("#/")){
+                    String s = (String)input.subSequence(input.indexOf("/#")+2,input.indexOf("#/"));
+                    Iterator i = Server.connect.entrySet().iterator();
+                    while (i.hasNext()) {
+                        Map.Entry pair = (Map.Entry)i.next();
+                        if(s.equals(pair.getKey())){
+                            Server.connect.get(pair.getKey()).println("MESSAGE"+pair.getKey() +" has whispered: " + input);
+                        }else{
+                            PrintWriter pp = Server.connect.get(pair.getKey());
+                            pp.write("MESSAGEUser has not been found...");
+                        }
+                    }
+                }else {
+                   connectIterate(input);
+                }
+            }
         }catch(IOException e){
             System.err.println(e);
+        }
+    }
+
+
+    private void connectIterate(String msg){
+        Iterator i = Server.connect.entrySet().iterator();
+        while (i.hasNext()) {
+            Map.Entry pair = (Map.Entry) i.next();
+            Server.connect.get(pair.getKey()).println("MESSAGE" + pair.getKey() +": "+ msg);
         }
     }
 
--- Verteilte Systeme/src/Server/Server.java
+++ Verteilte Systeme/src/Server/Server.java
@@ -7,8 +7,10 @@ import java.util.concurrent.Executors;
 
 public class Server {
 
-    static final HashSet<String> loginHash = new HashSet<>();
-    static HashSet<PrintWriter> printHash = new HashSet<>();
+//    static final HashSet<String> loginHash = new HashSet<>();
+//    static HashSet<PrintWriter> printHash = new HashSet<>();
+
+    static HashMap<String, PrintWriter> connect = new HashMap<>();
 
     public static void main(String[] args) throws Exception{
 
