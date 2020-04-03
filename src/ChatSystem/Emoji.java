ede5673f0b5b37a234330e6a7db74baf654f8aea
JeannineB <43883074+JeannineB@users.noreply.github.com>
2020-04-03 14:30:56 +0200

    Update Emoji.java

--- src/ChatSystem/Emoji.java
+++ src/ChatSystem/Emoji.java
@@ -3,6 +3,33 @@ package ChatSystem;
 public enum Emoji {
 
 	GRINNING_FACE("grinning-face.png", ":D");
+	BICEPS("Biceps.png", "biceps");
+	BROGLE("broccoli.png", "broccoli");
+	FIREFIGHTER("Eger.png", "Eger");
+	EXPLODINGHEAD("exploding.png", "explodinghead");
+	FLOWER("Flower.png", "flower");
+	HEART("Heart.png"; "<3");
+	HEARTEYES("heart_shaped_eyes.png", "<3.<3");
+	HEDGEHOG("hedgehog.png", "hedgehog");
+	HUNDRED("hundred.png", "100");
+	GOAT("Jeannine.png", "Jeannine");
+	CAT("kissingcat.png", "cat");
+	MONKEY("monkey.png", "monkey");
+	MOON("moon.png", "moon");
+	COOK("Moritz.png", "Moritz");
+	NAUSEATED("nauseated.png", "nauseated");
+	CALLME("Nico.png", "Nico");
+	OTTER("otter.png", "otter");
+	PEACH("peach.png", "butt");
+	PIZZA("pizza.png", "pizza");
+	POOP("poop.png", "poop");
+	PRINCESS("princess.png", "princess");
+	SPLASH("splash.png", "splash");
+	OK("Timo.png", "Timo");
+	TREX("t-rex.png", "trex");
+	HEARTS("two-hearts.png", "<33");
+	NAILS("Veli.png", "Veli");
+	
 	
 	public String name;
 	public String shortcut;
